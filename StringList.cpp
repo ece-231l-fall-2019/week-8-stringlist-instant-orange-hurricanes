905d236422809fabceff56dbbd082f6be657467d
Nigel Damian <ndamian4@unm.edu>
2019-10-11 04:37:34 -0600

    Segmentation fault (core dumped) (!) While Loops (!) Error :tree:

--- StringList.cpp
+++ StringList.cpp
@@ -1,13 +1,16 @@
 #include "StringList.h"
 
-size_t StringList::size() const noexcept
-{
+size_t StringList::size() const
+{ 
 	size_t n = 0;
-	
-	for( llist *newItem = new llist;  !newItem.empty()  ; n++)
+	llist *Copy = new llist;
+	Copy = _data;
+	for( llist *temp = Copy; Copy->next == NULL; n++)
 	{
-		pop_back();
+		temp = Copy->next;
+		Copy = temp->next;
 	}
+
 	return n;
 }
 
@@ -21,45 +24,80 @@ void StringList::clear() noexcept
 
 std::string& StringList::back()
 {
-	llist newItem = new llist;
-	newItem = 
-	for(size_t n = size(); n != 0 ;n--)
-	{	
-		newItem.pop_back();
-	}
-	return newItem.front();
+	while( _data->next != NULL )
+	{
+		llist *tail = _data;
+		tail = _data->next;
+		_data = tail;
+		if( _data->next == NULL )
+		{
+			return _data->str;
+		}
+	}	
+	return _data->str;
 }
 
-void StringList::push_back (std::string c);
+void StringList::push_back (std::string c)
 {
-	llist *newItem = new llist;
-	newItem->str = _data;
-	newItem->next = c;
-	_data = newItem;
+	llist *temp = _data;
+	while( _data->next != NULL )
+	{
+		temp = _data->next;
+		_data = temp;
+		if( _data->next == NULL )
+		{
+			_data->str = c;
+		}
+	}
+	_data->str = c;
 }
 
 void StringList::pop_back()
 {
 	llist *back = _data;
-	_data = back->next;
+	while( _data->next != NULL )
+	{
+		back = _data->next;
+		if( back->next == NULL)
+		{
+			delete back;
+		}
+		_data = back;
+	}
+	
 	delete back;	
 }
 
 void StringList::reserve(size_t new_cap)
-{
+{	
+
 	if( new_cap == size() )
-	{
-		break;
-	}
-	else if( new_cap > max_size())
+		return;	
+
+/*	else if( new_cap > max_size())
 		throw std::length_error("new_cap is greater than max_size");
+*/
 	else if( new_cap > size() )
 	{
-		// resize
+		llist *add_node = _data;
+		while( _data->next != 0)
+		{
+			add_node = _data->next;
+			_data = add_node;
+			if( _data->next == NULL)
+			{
+				for( size_t n = size(); n != 0; n-- )
+				{
+					_data = add_node->next;
+					add_node = _data;
+				}
+			}	 
+		}
 	}
 	else if( new_cap < size() )
 	{
-		for( size_t n = size(); n != new_cap; n--, pop_back())	
+		for( size_t n = size(); n != new_cap; n--)
+			pop_back();	
 	}
 	else
 		std::cout<< " Condition not meet " << std::endl;
